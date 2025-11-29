0ec0731eab4c3e160702e268b597da8f7399bb5e
oscaredu1226 <oscaredu01@hotmail.com>
2025-11-29 00:44:33 -0500

    chore: initial commit

---
+++ src/main/java/com/upc/tukuntechconfigserver/TukuntechConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.upc.tukuntechconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukuntechConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TukuntechConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/tukuntechconfigserver/TukuntechConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.tukuntechconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukuntechConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
